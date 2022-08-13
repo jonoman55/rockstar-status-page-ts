4f94cc967ff97796e1dc27c3cbf8556fb1c453fd
jonoman55 <jonoman55@yahoo.com>
2022-08-13 12:41:54 -0400

    comments and types added

--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -1,22 +1,52 @@
 import { useCallback, useState, useEffect } from "react";
 
-export const getItem = (value: string) => {
+/**
+ * Get Item
+ * @param {string} value Storage Item Value
+ * @returns {any} Storage Item
+ */
+export const getItem = (value: string): any => {
     return JSON.parse(localStorage.getItem(value) as string);
 };
 
-export const setItem = (key: string, value: string) => {
+/**
+ * Set Item
+ * @param key Storage Item Key
+ * @param value Storage Item Value
+ * @returns {any[]} Storage Value
+ */
+export const setItem = (key: string, value: string): string => {
     return JSON.stringify(localStorage.setItem(key, value));
 };
 
-export function useLocalStorage(key: string, defaultValue: any) {
+/**
+ * Use Local Storage
+ * @param {string} key Storage Item Key
+ * @param {any} defaultValue Storage Item Value
+ * @returns {any[]} Dependencies Array
+ */
+export function useLocalStorage(key: string, defaultValue: any): any[] {
     return useStorage(key, defaultValue, window.localStorage);
 };
 
-export function useSessionStorage(key: string, defaultValue: any) {
+/**
+ * Use Session Storage Hook
+ * @param {string} key Storage Item Key
+ * @param {any} defaultValue Storage Item Value
+ * @returns {any[]} Dependencies Array
+ */
+export function useSessionStorage(key: string, defaultValue: any): any[] {
     return useStorage(key, defaultValue, window.sessionStorage);
 };
 
-function useStorage(key: string, defaultValue: any, storageObject: any) {
+/**
+ * Use Storage Hook
+ * @param {string} key Storage Item Key
+ * @param {any} defaultValue Storage Item Value
+ * @param {any} storageObject Storage Object
+ * @returns {any[]} value, setValue, remove
+ */
+export const useStorage = (key: string, defaultValue: any, storageObject: any): any[] => {
     const [value, setValue] = useState(() => {
         const jsonValue = storageObject.getItem(key);
         if (jsonValue != null) return JSON.parse(jsonValue);
