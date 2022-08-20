a7e8386de6568694ee3c9a4b3748952994f88c5e
jonoman55 <jonoman55@yahoo.com>
2022-08-20 12:21:35 -0400

    Added PlatformsDataGrid component

---
+++ src/components/outages/PlatformsDataGrid.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// TODO : Implement this component
+export const PlatformsDataGrid = () => {
+    return (
+        <div>PlatformsDataGrid</div>
+    );
+};
--- src/components/outages/index.ts
+++ src/components/outages/index.ts
@@ -1,3 +1,4 @@
 export * from './OutagesCard';
 export * from './OutagesSpinner';
+export * from './PlatformsDataGrid';
 export * from './ServicesDataGrid';
