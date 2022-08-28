93d2503d59b9daccb6fd1f2a6b5868431d7530fd
jonoman55 <jonoman55@yahoo.com>
2022-08-28 18:12:23 -0400

    StatusesPage overall status Avatar updated

--- src/components/statuses/StatusesCard.tsx
+++ src/components/statuses/StatusesCard.tsx
@@ -1,24 +1,26 @@
-import { useMemo } from 'react';
-import { useTheme } from '@mui/material';
+import { useCallback, useMemo } from 'react';
+import { Theme, useTheme } from '@mui/material';
 
 import { RockstarSpinner } from '../design';
 import { StatusesGridItems } from './StatusesComponents';
 import { Paper, Card, CardHeader, CardMedia, CardFooter } from '../styled/PaperCard.styled';
 import { CardContent } from '../styled/StatusesCard.styled';
 import { useGetStatusesQuery } from '../../services/rockstarApi';
-import { RockstarStatus } from '../../constants';
-import { getStatusesCount } from '../../helpers';
+import { getHighestStatusCount } from '../../helpers';
 
-import type { Status, StatusType } from '../../types';
+import type { Status, StatusItem, StatusItems, StatusType } from '../../types';
 
 export const StatusesCard: React.FC = (): JSX.Element => {
-    const theme = useTheme();
+    const theme: Theme = useTheme();
 
     const { data: statusesResults, isLoading: statusesIsLoading, refetch } = useGetStatusesQuery('getStatuses', {
         refetchOnReconnect: true,
         pollingInterval: 1000 * 60 * 5 // 5 min
     });
 
+    /**
+     * Get Statuses
+     */
     const statuses: Status[] = useMemo<Status[]>(() => {
         const results: Status[] = [];
         if (!statusesIsLoading && statusesResults) {
@@ -29,18 +31,50 @@ export const StatusesCard: React.FC = (): JSX.Element => {
         return results;
     }, [statusesResults, statusesIsLoading]);
 
-    const overallStatus: StatusType = useMemo<StatusType>(() => {
-        let result: StatusType;
-        if (!statusesIsLoading && statuses) {
-            const highest = getStatusesCount(
-                statuses.map((s: Status) => {
-                    return s?.status?.toLowerCase() as RockstarStatus;
-                })
-            );
-            result = highest?.toString().toLowerCase() as StatusType;
-        }
-        return result;
-    }, [statuses, statusesIsLoading]);
+    /**
+     * Get Overall Status
+     */
+     const overallStatus: StatusType = useMemo<StatusType>(
+        () => {
+            // Initial State
+            let statusItems: StatusItems = {
+                statuses: []
+            };
+            // Add Service Statuses to state
+            if (!statusesIsLoading && statuses) {
+                statuses.forEach(
+                    (s) => statusItems.statuses.push({
+                        name: s.name,
+                        status: s.status.toLowerCase(),
+                    })
+                );
+            }
+            // Get All Statuses from state
+            const statusesValues: StatusItem[] = Object.values(statusItems.statuses);
+            // Get All Status Values
+            const allStatusValues = statusesValues.map((v) => v.status.toLowerCase());
+            // Check if Service and Status statuses are all UP
+            const isOverallAllUp: boolean = allStatusValues.every((v) => v === 'up');
+            // All UP
+            if (isOverallAllUp) return 'up';
+            // Service DOWN
+            if (allStatusValues.includes('down')) return 'down';
+            // Service LIMITED
+            if (allStatusValues.includes('limited')) return 'limited';
+            // Get highest status value count
+            const highestValue = getHighestStatusCount(allStatusValues) as StatusType;
+            // return highest status
+            return highestValue;
+        },
+        [statuses, statusesIsLoading]
+    );
+
+    /**
+     * Handle Refetch
+     */
+    const handleClick = useCallback<() => void>(() => {
+        refetch();
+    }, [refetch]);
 
     return statusesIsLoading ? <RockstarSpinner /> : (
         <Paper elevation={0}>
@@ -49,7 +83,7 @@ export const StatusesCard: React.FC = (): JSX.Element => {
                     title='Rockstar Statuses'
                     subheader={`${new Date().toLocaleString()}`}
                     status={overallStatus as StatusType}
-                    onClick={refetch}
+                    onClick={handleClick}
                 />
                 <CardMedia id={10} />
                 <CardContent>
