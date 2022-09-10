b95d178be789072e65f50f06b380816276f1b3b5
jonoman55 <jonoman55@yahoo.com>
2022-09-10 12:18:19 -0400

    axios package and fetch utils added

---
+++ src/utils/fetch.ts
@@ -0,0 +1,49 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
+type ApiUrl = {
+    id: number;
+    name: string;
+    url: string;
+};
+
+export const ApiUrls: ApiUrl[] = [
+    { id: 0, name: 'heroku', url: `${process.env.REACT_APP_BACKEND_API_HEROKU_URL}` },
+    { id: 1, name: 'render', url: `${process.env.REACT_APP_BACKEND_API_RENDER_URL}` },
+    { id: 2, name: 'railway', url: `${process.env.REACT_APP_BACKEND_API_RAILWAY_URL}` }
+];
+
+export const heroku: ApiUrl = ApiUrls[0];
+export const render: ApiUrl = ApiUrls[1];
+export const railway: ApiUrl = ApiUrls[2];
+
+export const herokuRequest = axios.get(heroku.url);
+export const renderRequest = axios.get(render.url);
+export const railwayRequest = axios.get(heroku.url);
+
+export const fetchApiUrls = async (): Promise<AxiosResponse<any, any>[] | undefined> => {
+    try {
+        const promises: Promise<AxiosResponse<any, any>>[] = ApiUrls.map(
+            ({ url }: ApiUrl) => axios.get(url)
+        );
+        return await Promise.all(promises);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const fetchAllUrls = (): Promise<void> => {
+    return axios.all([
+        herokuRequest,
+        renderRequest,
+        railwayRequest
+    ]).then(axios.spread((...responses: AxiosResponse<any, any>[]) => {
+        const responseOne = responses[0]
+        const responseTwo = responses[1]
+        const responseThree = responses[2]
+        // use/access the results 
+        console.log(responseOne, responseTwo, responseThree);
+    })).catch((error: AxiosError) => {
+        // react on errors.
+        console.log(error);
+    });
+};
