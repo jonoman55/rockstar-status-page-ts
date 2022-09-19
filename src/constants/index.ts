c53479944400c201a241e7635d0fa7f277b45c36
jonoman55 <jonoman55@yahoo.com>
2022-09-18 22:00:36 -0400

    moved urls from .env to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export * from './RockstarStatus';
 export * from './PlatformType';
 export * from './LinkItems';
 export * from './Routes';
+export * from './urls';
---
+++ src/constants/urls.ts
@@ -0,0 +1,9 @@
+// GitHub Repo URLs
+export const REACT_APP_GITHUB_URL = 'https://github.com/jonoman55';
+export const REACT_APP_GITHUB_REPO_URL = 'https://github.com/jonoman55/rockstar-status-page-ts';
+// Rockstar Support URLs
+export const REACT_APP_ROCKSTAR_URL = 'https://www.rockstargames.com';
+export const REACT_APP_ROCKSTAR_SERVICE_URL = 'https://support.rockstargames.com/servicestatus';
+export const REACT_APP_ROCKSTAR_TWITTER_URL = 'https://www.twitter.com/RockstarSupport';
+// Rockstar Service Status URL
+export const REACT_APP_ROCKSTAR_STATUS_URL = 'https://support.rockstargames.com/services/status.json?tz=America/New_York';
